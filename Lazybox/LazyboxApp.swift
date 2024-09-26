d03fa247e08e0bf82ba6a9bd38e5f37b5d3746ae
noViceMin <admin@isamin.kr>
2024-09-26 13:59:58 +0900

    Initial Commit

---
+++ Lazybox/LazyboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyboxApp.swift
+//  Lazybox
+//
+//  Created by noViceMin on 9/26/24.
+//
+
+import SwiftUI
+
+@main
+struct LazyboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
